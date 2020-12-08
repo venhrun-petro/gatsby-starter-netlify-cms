6d985a8f0fe4ad5c67ef3154c1bfc11152d58097
p.venhryn <p.venhryn@pozitiff.ua>
2020-12-08 12:57:53 +0200

    nn

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,63 +1,93 @@
+let path = require('path');
+
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
-    description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+    title: `Східниця Таксі`,
+    description: `gatsby-hospitality-core`,
+    author: `gatsby-hospitality-core`,
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
     {
-      // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/static/img`,
-        name: 'uploads',
+        name: `/images`,
+        path: `${__dirname}/static/`,
+      },
+    },
+    `gatsby-transformer-json`,
+     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `uk`,
+        path: `${__dirname}/content/uk/`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `en`,
+        path: `${__dirname}/content/en/`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
+        name: "pages",
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src/templates`,
+        name: "templates",
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-sharp`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `${__dirname}/static/icons/favicon.png`,
       },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-plugin-i18n",
       options: {
-        path: `${__dirname}/src/img`,
-        name: 'images',
+        langKeyForNull: `uk`,
+        langKeyDefault: `uk`,
+        useLangKeyLayout: false,
+        prefixDefault: false,
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: `gatsby-alias-imports`,
       options: {
-        plugins: [
-          {
-            resolve: 'gatsby-remark-relative-images',
-            options: {
-              name: 'uploads',
-            },
-          },
-          {
-            resolve: 'gatsby-remark-images',
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: 'gatsby-remark-copy-linked-files',
-            options: {
-              destinationDir: 'static',
-            },
-          },
-        ],
-      },
+        aliases: {
+          '~': path.resolve(__dirname, 'src'),
+          '~c': path.resolve(__dirname, 'src/components'),
+          '~p': path.resolve(__dirname, 'src/pages'),
+          '~s': path.resolve(__dirname, 'src/store'),
+          '~d': path.resolve(__dirname, 'content'),
+          '~r': path.resolve(__dirname, 'src/redux'),
+          '~h': path.resolve(__dirname, 'src/hooks'),
+          '~u': path.resolve(__dirname, 'src/utils'),
+          '~style': path.resolve(__dirname, 'src/styles'),
+          '~f': path.resolve(__dirname, 'src/fonts'),
+          '~img': path.resolve(__dirname, 'static/images'),
+          '~ic': path.resolve(__dirname, 'static/icons')
+        }
+      }
     },
     {
       resolve: 'gatsby-plugin-netlify-cms',
@@ -65,13 +95,6 @@ module.exports = {
         modulePath: `${__dirname}/src/cms/cms.js`,
       },
     },
-    {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    'gatsby-plugin-netlify', // make sure to keep it last in the array
+    'gatsby-plugin-netlify', 
   ],
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,4 @@
-const _ = require('lodash')
-const path = require('path')
-const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+const path = require("path")
 
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
@@ -11,77 +8,36 @@ exports.createPages = ({ actions, graphql }) => {
       allMarkdownRemark(limit: 1000) {
         edges {
           node {
-            id
             fields {
               slug
+              langKey
             }
             frontmatter {
-              tags
               templateKey
             }
           }
         }
       }
     }
-  `).then((result) => {
+  `).then(result => {
     if (result.errors) {
-      result.errors.forEach((e) => console.error(e.toString()))
+      result.errors.forEach(e => console.error(e.toString()))
       return Promise.reject(result.errors)
     }
-
+  
     const posts = result.data.allMarkdownRemark.edges
-
-    posts.forEach((edge) => {
-      const id = edge.node.id
-      createPage({
+    
+    posts.forEach(edge => {
+      const page = {
         path: edge.node.fields.slug,
-        tags: edge.node.frontmatter.tags,
-        component: path.resolve(
-          `src/templates/${String(edge.node.frontmatter.templateKey)}.js`
+        component: require.resolve(
+          `./src/templates/${String(edge.node.frontmatter.templateKey)}.js`
         ),
-        // additional data can be passed via context
         context: {
-          id,
+          langKey: edge.node.fields.langKey,
         },
-      })
-    })
-
-    // Tag pages:
-    let tags = []
-    // Iterate through each post, putting all found tags into `tags`
-    posts.forEach((edge) => {
-      if (_.get(edge, `node.frontmatter.tags`)) {
-        tags = tags.concat(edge.node.frontmatter.tags)
       }
-    })
-    // Eliminate duplicate tags
-    tags = _.uniq(tags)
-
-    // Make tag pages
-    tags.forEach((tag) => {
-      const tagPath = `/tags/${_.kebabCase(tag)}/`
-
-      createPage({
-        path: tagPath,
-        component: path.resolve(`src/templates/tags.js`),
-        context: {
-          tag,
-        },
-      })
+      createPage(page)
     })
   })
-}
-
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
-
-  if (node.internal.type === `MarkdownRemark`) {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      name: `slug`,
-      node,
-      value,
-    })
-  }
 }
